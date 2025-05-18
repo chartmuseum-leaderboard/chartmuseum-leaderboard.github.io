51521bf78db55594fead7fb9d2e530c0ca29cc57
Liyan06 <lytang06@gmail.com>
2025-05-18 00:28:07 -0400

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "ChartMuseum Leaderboard",
   description: "ChartMuseum Leaderboard",
+  icons: {
+    icon: '/site-logo.png',
+  },
 };
 
 export default function RootLayout({
